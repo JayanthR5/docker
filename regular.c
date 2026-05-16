8539ce535c096f9da8fdba5cc27893753fc4f34d
Jayanth R <jayanthramanna51@gmail.com>
2026-05-16 10:05:53 +0530

    done

--- regular.c
+++ regular.c
@@ -313,3 +313,88 @@ printf("%d\n", (ML > MR ? ML : MR));
 }  
 return 0;  
 } 
+
+
+pangram
+#include <stdio.h>
+#include <string.h>
+
+char s[10000];
+
+int main()
+{
+    gets(s);
+
+    int f[300] = {0}, ans = 0, i;
+
+    int l = strlen(s);
+
+    for(i = 0; i < l; i++)
+    {
+        if(s[i] == ' ')
+            continue;
+
+        if(s[i] >= 'a')
+            s[i] = s[i] - ('a' - 'A');
+
+        if(!(f[s[i]]++))
+            ans++;
+    }
+
+    if(ans != 26)
+        printf("not ");
+
+    printf("pangram\n");
+
+    return 0;
+}
+
+caesar cipher
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, i, j, k;
+    char ar[101];
+    unsigned char x;
+
+    scanf("%d", &n);
+    scanf("%s", ar);
+    scanf("%d", &k);
+
+    for(i = 0; i < n; i++)
+    {
+        x = ar[i];
+
+        if(x >= 97 && x <= 122)
+        {
+            x = x + (k % 26);
+
+            if(x > 122)
+            {
+                x = 96 + (x - 122);
+            }
+
+            ar[i] = x;
+        }
+        else if(x >= 65 && x <= 90)
+        {
+            x = x + (k % 26);
+
+            if(x > 90)
+            {
+                x = 64 + (x - 90);
+            }
+
+            ar[i] = x;
+        }
+    }
+
+    printf("%s", ar);
+
+    return 0;
+}
